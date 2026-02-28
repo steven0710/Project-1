361c0d43d20d8c7dece4daf44e938baceeb4df50
steven0710 <stevenhujuly10@gmail.com>
2026-02-27 18:25:32 -0600

    remove backend folder

--- backend/src/routes/user.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller";
-
-const userRouter = Router();
-
-userRouter.route("/register").post(registerUser);
-userRouter.route("/login").post(loginUser);
-export default userRouter;
